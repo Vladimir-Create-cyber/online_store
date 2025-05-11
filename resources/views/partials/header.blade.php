9ce09910c5614c1a4aaa450f2b6bcf44bedf3b03
Vladimir-create-cyber <vlaaadimir1@gmail.com>
2025-05-11 19:23:14 +0300

    Revert "create class cart and style header"
    This reverts commit 32c22d181da2c721a61f66edb78a5df3ede5aabc.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,8 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
 <header>
     <nav>
         <a href="{{ route('home') }}">Главная</a>
-        <form action="{{ route('product.search') }}" method="GET">
-            <input type="text" name="query" placeholder="Поиск товаров...">
-            <button type="submit">Найти</button>
-        </form>
-{{--        <a href="{{ route('product.search') }}">Поиск</a>--}}
-        <a class="cart" href="{{ route('cart.index') }}">Корзина</a>
+        <a href="{{ route('product.search') }}">Поиск</a>
+        <a href="{{ route('cart.index') }}">Корзина</a>
         {{-- Можно добавить ссылки на личный кабинет и регистрацию --}}
     </nav>
 </header>
